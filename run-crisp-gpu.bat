713e5fd2c1de601d1257d790ac06604cfd2b339b
Vivek Bhakta Mathema <42566129+vivekmathema@users.noreply.github.com>
2025-01-31 15:06:00 +0700

    Update run-crisp-gpu.bat

--- run-crisp-gpu.bat
+++ run-crisp-gpu.bat
@@ -2,5 +2,5 @@
 REM Running GRASP GPU version from internal python environment Windows 10 version
 REM Requires CUDA 11.0+ cuDNN 8.0+ library for running on NVIDIA RTX2000s GPUs
 REM "Reset Setting" option to start default configuration
-%~dp0.\python3gpu_env\scripts\python.bat crisp.pyc
+%~dp0.\python3gpu_env\scripts\python.bat crisp.py
 pause
